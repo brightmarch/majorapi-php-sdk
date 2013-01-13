a732d51af361639e392b2cdba59ac075b314d55a
Vic Cherubini <vmc@leftnode.com>
2013-01-12 19:28:36 -0600

    Initial beginnings of creating a customer and reading a customer.

---
+++ lib/MajorApiQuickbooks.php
@@ -0,0 +1,145 @@
+<?php
+
+require_once __DIR__ . '/MajorApiException.php';
+
+class MajorApiQuickbooks
+{
+
+    /** @var string */
+    private $applicationUsername = '';
+
+    /** @var string */
+    private $aplicationApiKey = '';
+
+    /** @var array */
+    private $urls = [];
+
+    /** @var string */
+    private $url = '';
+
+    private $successCodes = [200, 201, 202];
+
+    public function __construct($applicationUsername, $applicationApiKey)
+    {
+        $this->applicationUsername = strtolower($applicationUsername);
+        $this->applicationApiKey = $applicationApiKey;
+
+        $this->_configureUrls();
+    }
+
+    public function createCustomer(array $customer)
+    {
+        if (!array_key_exists('name', $customer)) {
+            throw new MajorApiException(
+                "The name attribute is required " .
+                "when creating a new customer."
+            );
+        }
+
+        return $this->_sendRequest('POST', 'customers', $customer);
+    }
+
+    public function retrieveCustomer($customerId)
+    {
+        $resource = sprintf('customers/%d', (int)$customerId);
+
+        return $this->_sendRequest('GET', $resource);
+    }
+
+    public function enableProduction()
+    {
+        $this->url = $this->urls['production'];
+
+        return $this;
+    }
+
+    public function enableStaging()
+    {
+        $this->url = $this->urls['staging'];
+
+        return $this;
+    }
+
+    public function enableDevelopment()
+    {
+        $this->url = $this->urls['development'];
+
+        return $this;
+    }
+
+    public function getUrl()
+    {
+        if (empty($this->url)) {
+            throw new MajorApiException(
+                "No URL is configured. Please call one of " .
+                "enableProduction(), enableStaging(), or " .
+                "enableDevelopment() to configure the proper URL."
+            );
+        }
+
+        return $this->url;
+    }
+
+
+
+
+
+
+
+    private function _sendRequest($method, $resource, array $entity=[])
+    {
+        $url = sprintf(
+            '%s%s',
+            $this->getUrl(),
+            strtolower($resource)
+        );
+
+        $authorization = sprintf(
+            '%s:%s',
+            $this->applicationUsername,
+            $this->applicationApiKey
+        );
+
+        $options = [
+            CURLOPT_CUSTOMREQUEST => strtoupper($method),
+            CURLOPT_HEADER => 0,
+            CURLOPT_HTTPHEADER => ['Accept: application/json', 'Expect:'],
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_SSL_VERIFYHOST => 2,
+            CURLOPT_TIMEOUT => 5,
+            CURLOPT_URL => $url,
+            CURLOPT_USERPWD => $authorization
+        ];
+
+        if (is_array($entity) && count($entity) > 0) {
+            $options[CURLOPT_POSTFIELDS] = http_build_query($entity);
+        }
+
+        $curl = curl_init();
+        curl_setopt_array($curl, $options);
+        $response = json_decode(curl_exec($curl));
+        $responseCode = (int)curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        if (!in_array($responseCode, $this->successCodes)) {
+            throw new MajorApiException(sprintf(
+                "An error occurred when attempting to contact " .
+                "the MajorApi: QuickBooks REST API. The error states: %s",
+                $response->message
+            ));
+        }
+
+        return $response;
+    }
+
+    private function _configureUrls()
+    {
+        $this->urls = [
+            'production' => 'https://majorapi.com/api/quickbooks/',
+            'staging' => 'https://staging.majorapi.com/api/quickbooks/',
+            'development' => 'http://localhost:8000/api/quickbooks/'
+        ];
+
+        return $this;
+    }
+
+}
---
+++ tests/MajorApiQuickbooksTest.php
@@ -0,0 +1,61 @@
+<?php
+
+require_once __DIR__ . '/../lib/MajorApiQuickbooks.php';
+
+class MajorApiQuickbooksTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testEnablingProduction()
+    {
+        $maq = new MajorApiQuickbooks('username', 'apikey');
+        $maq->enableProduction();
+
+        $this->assertEquals('https://majorapi.com/api/quickbooks/', $maq->getUrl());
+    }
+
+    public function testEnablingStaging()
+    {
+        $maq = new MajorApiQuickbooks('username', 'apikey');
+        $maq->enableStaging();
+
+        $this->assertEquals('https://staging.majorapi.com/api/quickbooks/', $maq->getUrl());
+    }
+
+    public function testEnablingDevelopment()
+    {
+        $maq = new MajorApiQuickbooks('username', 'apikey');
+        $maq->enableDevelopment();
+
+        $this->assertEquals('http://localhost:8000/api/quickbooks/', $maq->getUrl());
+    }
+
+    /**
+     * @expectedException MajorApiException
+     */
+    public function testCreatingCustomerRequiresConfiguredUrl()
+    {
+        $maq = new MajorApiQuickbooks('username', 'apikey');
+        $maq->createCustomer(['name' => 'Maynard James Keenan']);
+    }
+
+    /**
+     * @expectedException MajorApiException
+     */
+    public function testCreatingCustomerRequiresNameAttribute()
+    {
+        $maq = new MajorApiQuickbooks('username', 'apikey');
+        $maq->createCustomer([]);
+    }
+
+    /**
+     * @expectedException MajorApiException
+     */
+    public function testCreatingCustomerRequiresNoErrors()
+    {
+        $maq = new MajorApiQuickbooks('username', 'apikey');
+        $maq->enableDevelopment();
+
+        $maq->createCustomer(['name' => 'Maynard James Keenan Ivory Waynes World of Mars']);
+    }
+
+}
